033b2c03b637ff3b2513ad140eb972e13330e660
Leandro Furtado <le_furtado@outlook.com.br>
2022-06-30 20:49:44 -0300

    Corrigida as rotas CRUD do projeto

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/usuario', (req, res) => res.send("Olá mundo!"));
-app.post('/usuario', (req, res) => res.send({nome: 'Leandro', sobrenome: 'Furtado'}));
-app.put('/usuario:id', (req, res) => res.send({nome: 'Furtado', sobrenome: 'Leandro'}));
-app.delete('/usuario:id', deleteUsuario, (req, res) => {
-    const { id } = req.params;
-});
-//criando um servidor
+//Rota para requisição de página /usuario
+app.get('/usuario', (req, res) => res.send("Bem vindo usuario"));
+//Rota para incluir um usuário
+app.post('/usuario', (req, res) => res.send('Incluir um novo usuário'));
+//Rota para atualizar um usuário
+app.put('/usuario/:id', (req, res) => res.send(`Editar o usuario ${req.params.id}`));
+//Rota para deletar um usuário
+app.delete('/usuario/:id', (req, res) => res.send(`Excluir o usuario ${req.params.id}`));
 
 app.listen(3000, () => console.log("Servidor rodando"));
